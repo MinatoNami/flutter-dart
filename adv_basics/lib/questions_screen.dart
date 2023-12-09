3f38a844b9548cbd418cf5bd107c607aaf0e039b
Chong Cheng Yang, Lionel <chonglionel97@gmail.com>
2023-12-10 01:21:10 +0800

    Create question, answers and added styling

--- adv_basics/lib/questions_screen.dart
+++ adv_basics/lib/questions_screen.dart
@@ -1,7 +1,12 @@
 import 'package:flutter/material.dart';
 
+import 'package:adv_basics/answer_button.dart';
+import 'package:adv_basics/data/questions.dart';
+
 class QuestionsScreen extends StatefulWidget {
   const QuestionsScreen({super.key});
+
+  void onPressed() {}
 
   @override
   State<QuestionsScreen> createState() => _QuestionsScreenState();
@@ -10,6 +15,35 @@ class QuestionsScreen extends StatefulWidget {
 class _QuestionsScreenState extends State<QuestionsScreen> {
   @override
   Widget build(BuildContext context) {
-    return const Text('Questions Screen');
+    final currentQuestion = questions[0];
+
+    return SizedBox(
+      width: double.infinity,
+      child: Container(
+        padding: const EdgeInsets.all(40),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Text(
+              currentQuestion.question,
+              style: const TextStyle(
+                color: Colors.white,
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 30,
+            ),
+            ...currentQuestion.getShuffledAnswers().map((answer) {
+              return AnswerButton(
+                answerText: answer,
+                onPressed: () {},
+              );
+            })
+          ],
+        ),
+      ),
+    );
   }
 }
